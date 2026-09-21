9602f9073a6f98d09a06938c58c96f94210dc4f8
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2026-09-21 04:54:17 -0400

    Add `Printable` concept and `operator<<`` overload to allow printing objects with a `Print` function

--- scribe/utility/strings.h
+++ scribe/utility/strings.h
@@ -1,4 +1,25 @@
 #pragma once
+#include <ostream>
 #include <string>
 
 void ReplaceAll(std::string& source, const std::string& from, const std::string& to);
+
+template<typename T>
+concept Printable = requires(std::ostream& os, T const & t) {
+    { t.Print(os) };
+};
+
+template<Printable T>
+std::ostream& operator<<(std::ostream& os, const T& t) {
+    t.Print(os);
+    return os;
+}
+
+template <typename DepthT, typename T>
+std::ostream& PrintAtDepth(std::ostream& os, const DepthT depth, T&& arg)
+{
+    for (DepthT i = 0; i < depth; ++i)
+        os << "|  ";
+
+    return os << arg;
+}
